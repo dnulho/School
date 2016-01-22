cb3cf1305c24508a9fda7d28004ae0ed35400a9a
dnulho <nevet.dnulho@gmail.com>
2016-01-22 14:42:19 -0800

    Start creating empty functions and Documentation.

---
+++ PE 12_20_2 Steven Ohlund/PE 12_20_2 Steven Ohlund/PE-12_20_2.cpp
@@ -0,0 +1,71 @@
+/*******************************************************************************
+* Author: Steven Ohlund
+* Filename: PE-12_20_2.cpp
+* Created: 1.22.16
+* Modified: none
+*******************************************************************************/
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+//Function Declaration
+void GetTextFromFile();
+void FindWord();
+void CountWords();
+void PrintReport();
+
+/*******************************************************************************
+* Purpose: Write a program that reads in a text file one word at a time. Store
+* that word into a dynamically created array when it is first encountered. Create
+* a parallel integer array to hold the count of the number of times that each word
+* appears in the text file.
+*
+* Input: User input as necessary.
+*		 Text file to count the words contained inside.
+*
+* Output: Print a report to the screen stating each word that was found and how
+* many times it was found.
+*
+*******************************************************************************/
+int main()
+{
+
+	return 0;
+}
+
+/*******************************************************************************
+* 
+*
+*******************************************************************************/
+void GetTextFromFile()
+{
+
+}
+
+/*******************************************************************************
+*
+*
+*******************************************************************************/
+void FindWord()
+{
+
+}
+
+/*******************************************************************************
+*
+*
+*******************************************************************************/
+void CountWords()
+{
+
+}
+
+/*******************************************************************************
+*
+*
+*******************************************************************************/
+void PrintReport()
+{
+
+}
